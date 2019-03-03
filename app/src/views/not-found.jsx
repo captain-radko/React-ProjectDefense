807ea2dfc668c1afb275e80809d96c2c3fa73601
capitan-radko <water235641@abv.bg>
2019-03-03 16:07:24 +0200

    Added default home and 404 views

---
+++ app/src/views/not-found.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NotFound extends Component{
+    render(){
+        return (
+            <div>
+                <h1>Your are playing wrong chords! Go back and try again!</h1>
+            </div>
+        )
+    }
+}
+
+export default NotFound;
